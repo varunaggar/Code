4b611c66577eb920e4195058549204255238d467
vaggar123 <165725291+vaggar123@users.noreply.github.com>
2026-02-01 19:18:53 +0000

    dels

--- Powershell/Generic/TestEnsureModules.ps1
+++
@@ -1,7 +0,0 @@
-Import-Module -Force "$PSScriptRoot/Modules/Common/Common.psm1"
-
-Write-Host "Testing Ensure-Modules for local module 'Foo'..."
-Ensure-Modules -ModuleNames 'Foo' | Out-Null
-
-$result = Get-Foo
-Write-Host "Get-Foo returned: $result"
--- Powershell/NFRI19/Modules/Common/Common.psm1
+++
@@ -1,8 +0,0 @@
-<#
-DEPRECATED DUPLICATE MODULE
-This file was a corrupted copy of the authoritative Common module.
-Do NOT use or modify here. Import the canonical module instead:
-  Import-Module "$PSScriptRoot/../../Modules/Common/Common.psm1"
-Attempting to load this file will throw.
-#>
-throw "Deprecated duplicate 'Common.psm1' in NFRI19. Use Powershell/Modules/Common/Common.psm1 instead."
